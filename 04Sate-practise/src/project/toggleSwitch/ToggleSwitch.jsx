86bcd0448bd03f5b3e738fed5fa913cafc59217b
Mr Jung <jungdaulat99@gmail.com>
2025-07-08 00:29:37 +0545

    with variable

--- 04Sate-practise/src/project/toggleSwitch/ToggleSwitch.jsx
+++ 04Sate-practise/src/project/toggleSwitch/ToggleSwitch.jsx
@@ -8,24 +8,19 @@ const ToggleSwitch = () => {
     setIsOn(!isOn);
   };
 
+  const container = `"h-[100px] w-[200px] p-2 bg-gray-300 rounded-full flex items-center cursor-pointer select-none",
+        ${isOn ? "bg-green-500" : ""}`;
+
+  const toggleSwitch = `" h-[80px] w-[80px] rounded-full  flex items-center justify-center text-white text-2xl font-semibold  transition-transform duration-300 ease-in",
+          ${
+            isOn
+              ? "bg-green-500 border-4 border-white translate-x-[100px]"
+              : "bg-red-500 translate-x-0"
+          }`;
+
   return (
-    <div
-      className={clsx(
-        "h-[100px] w-[200px] bg-gray-300 rounded-full flex items-center cursor-pointer select-none",
-        isOn ? "bg-green-500" : ""
-      )}
-      onClick={handleToggleSwitch}
-    >
-      <span
-        className={clsx(
-          " h-[80px] w-[80px] rounded-full ml-[10px] flex items-center justify-center text-white text-2xl font-semibold  transition-transform duration-300",
-          isOn
-            ? "bg-green-500 border-4 border-white translate-x-[100px]"
-            : "bg-red-500 translate-x-0"
-        )}
-      >
-        {isOn ? "ON" : "OFF"}
-      </span>
+    <div className={clsx(container)} onClick={handleToggleSwitch}>
+      <span className={clsx(toggleSwitch)}>{isOn ? "ON" : "OFF"}</span>
     </div>
   );
 };
