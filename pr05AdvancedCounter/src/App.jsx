185863177d9f47159dfe0d8f8f1ed4d9adcd0d58
Mr Jung <jungdaulat99@gmail.com>
2025-07-12 22:30:33 +0545

    Advanced counter

--- pr05AdvancedCounter/src/App.jsx
+++ pr05AdvancedCounter/src/App.jsx
@@ -1,7 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
 
 const App = () => {
-  return <div></div>;
+  const [count, setCount] = useState(0);
+  const [step, setStep] = useState(1);
+
+  const increment = () => {
+    count < 20 ? setCount(count + step) : "";
+  };
+
+  const decrement = () => {
+    count > 0 ? setCount((prev) => prev - step) : "";
+  };
+
+  const reset = () => {
+    setCount(0);
+  };
+
+  return (
+    <div className="border w-full mx-auto max-w-md p-3">
+      <section className="flex flex-col gap-3 mb-3">
+        <p>
+          Count:<span className="text-lg font-semibold pl-1">{count}</span>
+        </p>
+        <p>
+          Step:{" "}
+          <input
+            onChange={(e) => setStep(Number(e.target.value))}
+            type="number"
+            className="border outline-none px-1"
+          />
+        </p>
+      </section>
+      <section className="flex gap-4 items-center justify-center">
+        <button
+          onClick={increment}
+          className="bg-black text-white px-1.5 cursor-pointer"
+        >
+          Increament
+        </button>
+        <button
+          onClick={decrement}
+          className="bg-black text-white px-1.5 cursor-pointer"
+        >
+          Decreament
+        </button>
+        <button
+          onClick={reset}
+          className="bg-black text-white px-1.5 cursor-pointer"
+        >
+          Reset
+        </button>
+      </section>
+    </div>
+  );
 };
 
 export default App;
