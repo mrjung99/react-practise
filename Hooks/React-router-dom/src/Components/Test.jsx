0617dcdef487b5a66f781edfaf8f9835628f86f7
Mr Jung <jungdaulat99@gmail.com>
2025-07-30 20:59:18 +0545

    test

---
+++ Hooks/React-router-dom/src/Components/Test.jsx
@@ -0,0 +1,28 @@
+// import React from "react";
+// import { createBrowserRouter, RouterProvider } from "react-router-dom";
+// import PageLayout from "./layout/PageLayout";
+// import Home from "../pages/Home";
+// import About from "../pages/About";
+
+// const Test = () => {
+//   const router = createBrowserRouter([
+//     {
+//       path: "/",
+//       element: <PageLayout />,
+//       children: [
+//         {
+//           path: "/home",
+//           element: <Home />,
+//         },
+//         {
+//           path: "/about",
+//           element: <About />,
+//         },
+//       ],
+//     },
+//   ]);
+
+//   return <RouterProvider router={router} />;
+// };
+
+// export default Test;
