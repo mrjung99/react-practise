bceceadfaba984b914db90ca3ce534830ad000e4
Mr Jung <jungdaulat99@gmail.com>
2025-11-11 13:28:36 +0545

    crud with axios

---
+++ CRUD-WITH AXIOS/crud-axios/src/App.jsx
@@ -0,0 +1,11 @@
+import Posts from "./coponents/Posts";
+
+const App = () => {
+  return (
+    <section className="bg-gray-900 py-6">
+      <Posts />
+    </section>
+  );
+};
+
+export default App;
---
+++ CRUD-WITH AXIOS/crud-axios/src/coponents/Posts.jsx
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from "react";
+import { getData } from "../api/PostApi";
+
+const Posts = () => {
+  const [data, setData] = useState([]);
+
+  const getApiData = async () => {
+    try {
+      const res = await getData();
+      setData(res.data);
+      console.log(res.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getApiData();
+  }, []);
+  return (
+    <section className="max-w-6xl mx-auto">
+      <ul className="grid grid-cols-3 gap-7 text-justify">
+        {data.map((currData) => {
+          const { id, title, body } = currData;
+          return (
+            <li
+              key={id}
+              className="bg-gray-800 flex flex-col text-gray-300 gap-3 p-3 rounded-lg border-l border-gray-500"
+            >
+              <p>Title: {title}</p>
+              <p>Body: {body}</p>
+              <div className="flex gap-4">
+                <button className="bg-green-600 hover:bg-green-500 text-white px-4 py-1 rounded cursor-pointer">
+                  Edit
+                </button>
+                <button className="bg-orange-600 hover:bg-orange-500 text-white px-4 py-1 rounded cursor-pointer">
+                  Delete
+                </button>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
+    </section>
+  );
+};
+
+export default Posts;
