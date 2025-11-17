c109ffc7ccabd08e820950ae34219976671d4af4
Mr Jung <jungdaulat99@gmail.com>
2025-11-18 00:26:02 +0545

    crud

--- CRUD/Crud-operation/src/component/Form.jsx
+++ CRUD/Crud-operation/src/component/Form.jsx
@@ -8,6 +8,7 @@ const Form = ({ data, setData, updatePost, setUpdatePost }) => {
   });
 
   //* get updated info
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   useEffect(() => {
     if (updatePost) {
       setAddPost({
