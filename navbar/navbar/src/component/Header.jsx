68d48fd2e159ecea198720c2eeae21830b73c0e6
zeroCool <jungdaulat99@gmail.com>
2025-11-21 10:01:02 -0800

    navbar

--- navbar/navbar/src/component/Header.jsx
+++ navbar/navbar/src/component/Header.jsx
@@ -14,9 +14,18 @@ const Header = () => {
         <p className="text-3xl font-semibold text-orange-700">Mr Jung</p>
       </div>
 
-      <nav className="absolute top-[60px] left-0 bg-gray-100 w-full text-center shadow-md">
-        <ul className={isOpen ? "flex flex-col md:flex-row gap-7" : "hidden"}>
-          <li className="hover:text-blue-500 transition-colors cursor-pointer mt-2">
+      <nav
+        className="absolute md:static top-[60px] md:top-0 left-0 md:left-auto bg-gray-100 md:bg-transparent w-full 
+              md:w-auto text-center md:text-left shadow-md md:shadow-none"
+      >
+        <ul
+          className={
+            isOpen
+              ? "flex items-center flex-col md:flex-row gap-3"
+              : "hidden items-center md:flex md:flex-row gap-7"
+          }
+        >
+          <li className="hover:text-blue-500 transition-colors cursor-pointer mt-2 md:mt-0">
             Home
           </li>
           <li className="hover:text-blue-500 transition-colors cursor-pointer">
@@ -25,7 +34,7 @@ const Header = () => {
           <li className="hover:text-blue-500 transition-colors cursor-pointer">
             Service
           </li>
-          <li className="hover:text-blue-500 transition-colors cursor-pointer mb-2">
+          <li className="hover:text-blue-500 transition-colors cursor-pointer mb-2 md:mb-0">
             Contact
           </li>
         </ul>
--- navbar/navbar/src/component/HeroSection.jsx
+++ navbar/navbar/src/component/HeroSection.jsx
@@ -3,8 +3,8 @@ import React from "react";
 const HeroSection = () => {
   return (
     <div className="h-lvh flex flex-col md:flex-row items-center p-3 md:px-12 font-urbanist">
-      <div>
-        <p className="text-2xl md:text-4xl font-bold leading-normal">
+      <div className="order-2 md:order-1">
+        <p className="text-3xl md:text-4xl font-bold leading-normal">
           All your files in one secure location
         </p>
         <p className="">
@@ -12,7 +12,12 @@ const HeroSection = () => {
           whenever needed, share and collobrate with your connections.
         </p>
       </div>
-      <img src="/public/secure.png" alt="" className="w-xl" />
+
+      <img
+        src="/public/secure.png"
+        alt=""
+        className="order-1 md:order-2  w-xl"
+      />
     </div>
   );
 };
